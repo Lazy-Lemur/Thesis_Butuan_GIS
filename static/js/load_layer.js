d5958585e6723cffac733f5cf40b15db2dce1321
Jayon Jay Perater <jesungperater@gmail.com>
2022-01-08 02:24:58 +0800

    [GIS-011] - Fetched JSON data from database

--- static/js/load_layer.js
+++ static/js/load_layer.js
@@ -31,5 +31,123 @@ function load_layer() {
     value_param = param.options[param.selectedIndex].value;
     console.log(typeof(start_date));
 
+    var url_max = "../dbase/bxu_max_value_spatial.jsp";
+    url_max += "?parameter="+value_param;
+
+    var url_point = "../dbase/bxu_layer_point_spatial.jsp";
+    url_point += "?parameter="+value_param;
+
+    var url_poly = "../dbase/bxu_layer_spatial.jsp";
+    url_poly += "?parameter="+value_param;
+
+    $.getJSON(url_max, function(data){
+        //alert('karan');
+        var max_value = data.maximum[0].max;
+        console.log(max_value);
+        //alert(max_value);
+        
+        var diff = max_value/8;
+        //alert(diff);
+        
+        var i;
+        var k;
+        var color = [[254, 217, 118, 0.7], [254, 178, 76, 0.7], [253, 141, 60, 0.7], [252, 78, 42, 0.7], [227, 26, 28, 0.7], [189, 0, 38, 0.7], [128, 0, 38, 0.7] ];
+        
+        getStyle1 = function (feature, resolution) {
+        
+            for (i = 0; i < 7; i++) {
+            
+                if (feature.get([value_param]) > (i*diff) && feature.get([value_param]) <= ((i+1)*diff)) {
+                        return new ol.style.Style({
+                            fill: new ol.style.Fill({
+                                color: color[i] // semi-transparent red
+                            }),
+                            stroke: new ol.style.Stroke({
+                            color: 'white',
+                            lineDash: [2],
+                            width: 2
+                            })
+                        });
+                    }
+        //alert(i);
+                if (value_param == 'population' || value_param == 'employed'){
+                    $("#"+i).html(Math.round((i*diff)/1000)+"K - "+Math.round(((i+1)*diff)/1000)+"K");
+                
+                }
+                else{
+                    $("#"+i).html(Math.round((i*diff)/1000)+"K - "+Math.round(((i+1)*diff)/1000)+"K");
+                }
+                    $("#legend_title").html('<b>Legend - Layer1 '+value_param+'</b>');
+                }
+                if (feature.get([value_param]) == 0) {
+                    return new ol.style.Style({
+                        fill: new ol.style.Fill({
+                            color: [254, 217, 118, 0.7] // semi-transparent red
+                        }),
+                        stroke: new ol.style.Stroke({
+                        color: 'white',
+                        lineDash: [2],
+                        width: 2
+                        })
+                    });
+                }
+        };
+            
+            var col = 'rgba(255, 255, 0, 0.6)';
+            var col1 = 'rgba(255, 0, 0, 0.6)';
+            getStyle2 = function (feature, resolution) {
+            
+            
+            
+            var txt = new ol.style.Text({
+                text: feature.get('barangay')+":"+feature.get([value_param]),
+                offsetX: 20,
+                offsetY: -15,
+                font: '12px Calibri,sans-serif',
+                fill: new ol.style.Fill({
+                color: '#000'
+                }),
+                stroke: new ol.style.Stroke({
+                color: '#fff',
+                width: 3
+                })
+            });
+        
+            var fill = new ol.style.Fill({color: col});
+            var stroke = new ol.style.Stroke({color: col1, width: 1});
+            
+            
+            
+            for (i = 0; i < 7; i++) {
+            
+            if (feature.get([value_param]) > (i*diff) && feature.get([value_param]) <= ((i+1)*diff)) {
+                return new ol.style.Style({
+                    image: new ol.style.Circle({
+                    radius: 5*(i+1),
+                    fill: fill,
+                    stroke: stroke
+                    }),
+                //	text: txt
+                        
+                    });
+                }
+            //alert(i);
+            
+            }
+            if (feature.get([value_param]) == 0) {
+            return new ol.style.Style({
+                    image: new ol.style.Circle({
+                        radius: 0,
+                        fill: fill,
+                        stroke: stroke
+                    }),
+                        //text: txt
+                        
+                });
+            }
+    
+        };
+    
+    });
 }
 
