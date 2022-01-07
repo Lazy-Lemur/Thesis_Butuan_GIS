eff77694281015bee03deef46dd421e10c13df9d
Jayon Jay Perater <jesungperater@gmail.com>
2022-01-08 02:08:41 +0800

    [GIS-010] - Added JSP for initial db fetching

--- static/js/load_layer.js
+++ static/js/load_layer.js
@@ -24,4 +24,12 @@ function load_layer() {
         geojson_point.getSource().clear();
         overlays.getLayers().remove(geojson_point);
     }
+
+    var start_date = document.getElementById("start_date").value;
+    var end_date = document.getElementById("end_date").value;
+    var param = document.getElementById("parameter");
+    value_param = param.options[param.selectedIndex].value;
+    console.log(typeof(start_date));
+
 }
+
